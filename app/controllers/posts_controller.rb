6e7c10c14caf37d38325743fb5f2779d427ec792
Naokichi55 <ibosanganba@gmail.com>
2023-02-19 15:56:03 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,16 @@ class PostsController < ApplicationController
   end
 
   def new
+
+    @post = Post.new
+
   end
 
   def create
+
+    post = Post.create!(post_params)
+    rederect_to post
+
   end
 
   def update
@@ -16,4 +23,11 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+
 end
